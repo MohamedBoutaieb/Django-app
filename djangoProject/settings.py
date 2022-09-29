efa124eb7cfb1ded6c6a6c7e9c6f8ab007286a4e
MohamedBoutaieb <mohamedboutaieb1459@gmail.com>
2022-09-29 02:31:21 +0100

    updates

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6-_*d(hd1)^lg!3$3y99(xem^xgqpe86mh!3r&gi7pf+mhxpbt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
 ALLOWED_HOSTS = ['localhost','ddjango-todo-list.herokuapp.com']
 
