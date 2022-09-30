a5f42c7ec008a49e043aecb19412a66c00d38051
MohamedBoutaieb <mohamedboutaieb1459@gmail.com>
2022-09-30 14:57:41 +0100

    read me + adding wallet

--- ReadMe.md
+++ ReadMe.md
@@ -3,7 +3,7 @@
 <H1>Description</H1>
 This project is meant to demonstrate different Django functionalities from the view , models ,services, routing etc..
 
-<H1>Content</H1>
+<H1>Use Case</H1>
 <h3>To do List</h3>
 <ul>
 <li> create new to do lists </li>
@@ -13,7 +13,23 @@ This project is meant to demonstrate different Django functionalities from the v
 
 <h3>Bank account</h3>
 <ul>
-<li> create new to do lists </li>
-<li> create items inside the list , check/uncheck them , update and delete them </li>
-<li> access my to do list and modify it </li>
+<li> recharge your wallet </li>
+<li> send money from wallet to bank account </li>
+<li> withdraw money from wallet or bank account </li>
 </ul>
+
+<h3>Admin Dashboard  </h3>
+You can access admin dashboard via /admin 
+<hr>
+superuser: mohamedboutaieb <br>
+password : 1234
+
+<h4> How to run the project locally </h4>
+<ol>
+<li>clone the project</li>
+<li> execute the following commands on the root terminal</li>
+ python manage.py makemigrations   <br>
+ python manage.py migrate <br>
+python manage.py runserver <br>
+<li>got to localhost:8000</li>
+</ol>
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item, BankAccount
 from .forms import CreateNewList, Bank
+from django.contrib import messages
 
 
 # Create your views here.
@@ -36,7 +37,7 @@ def home(response):
 
 
 def create(response):
-    ls =  ToDoList.objects.all()
+    ls = ToDoList.objects.all()
     if (response.method == "POST"):
         form = CreateNewList(response.POST)
         if response.POST.get("del"):
@@ -48,15 +49,16 @@ def create(response):
             t.save()
         return HttpResponseRedirect("/%i" % t.id)
     form = CreateNewList
-    return render(response, "main/create.html", {"form": form,"todos":ls})
+    return render(response, "main/ToDo.html", {"form": form, "todos": ls})
 
 
 def bank(response):
     acc = BankAccount.objects.get(id=1)
-    if acc.getBalance() < 0 :
-        blc="rouge"
+    wal = BankAccount.objects.get(id=2)
+    if acc.getBalance() < 0:
+        blc = "rouge"
     else:
-        blc="vert"
+        blc = "vert"
     if (response.method == "POST"):
         if acc.getBalance() < 0:
             blc = "rouge"
@@ -65,12 +67,44 @@ def bank(response):
         form = Bank(response.POST)
         if response.POST.get("add"):
             if form.is_valid():
-                acc.balance+= form.cleaned_data["balance"]
+                acc.balance += form.cleaned_data["balance"]
                 acc.save()
         if response.POST.get("withdraw"):
             if form.is_valid():
-                acc.balance-= form.cleaned_data["balance"]
+                acc.balance -= form.cleaned_data["balance"]
+                acc.save()
+        if response.POST.get("add-wal"):
+            if form.is_valid():
+                wal.balance += form.cleaned_data["balance"]
+                wal.save()
+        if response.POST.get("withdraw-wal"):
+            if form.is_valid():
+                wal.balance -= form.cleaned_data["balance"]
+                if (wal.balance < 0):
+                    wal.balance += form.cleaned_data["balance"]
+                    messages.info(response, 'You can\'t withdraw more than you have in your wallet')
+                wal.save()
+        if response.POST.get("send-wal"):
+            if form.is_valid():
+                wal.balance -= form.cleaned_data["balance"]
+                if (wal.balance < 0):
+                    wal.balance += form.cleaned_data["balance"]
+                    messages.info(response, 'You can\'t withdraw more than you have in your wallet')
+                else :
+                    acc.balance += form.cleaned_data["balance"]
+                wal.save()
+                acc.save()
+        if response.POST.get("send-acc"):
+            if form.is_valid():
+
+                if (acc.balance <  form.cleaned_data["balance"]):
+
+                    messages.info(response, 'You can\'t send this amount because you\'re broke')
+                else :
+                    acc.balance -= form.cleaned_data["balance"]
+                    wal.balance += form.cleaned_data["balance"]
+                wal.save()
                 acc.save()
         return HttpResponseRedirect("bank")
     form = Bank
-    return render(response, "main/bank.html", {"bank": form, "account": acc,"blc":blc})
+    return render(response, "main/bank.html", {"bank": form, "account": acc, "blc": blc, "wallet": wal})
